966a30c2026b913c7c5e1cab8825aed6efa23494
Craig Thacker <craigthackerx@gmail.com>
2026-03-29 00:41:45 +0000

    Add schedule etc

--- modules/foundation/main.tf
+++ modules/foundation/main.tf
@@ -19,9 +19,9 @@ module "user_assigned_managed_identity" {
   source  = "libre-devops/user-assigned-managed-identity/azurerm"
   version = "~> 2.0.6"
 
-  rg_name  = module.shared_vars.foundation_rg_name
-  location = module.shared_vars.location
-  tags     = module.shared_vars.tags
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
 
   user_assigned_managed_identities = [
     {
@@ -34,9 +34,9 @@ module "automation_account" {
   source  = "libre-devops/automation-account/azurerm"
   version = "~> 1.0.0"
 
-  rg_name  = module.shared_vars.foundation_rg_name
-  location = module.shared_vars.location
-  tags     = module.shared_vars.tags
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
 
   automation_account_name       = module.shared_vars.foundation_aa_name
   public_network_access_enabled = true
@@ -88,4 +88,72 @@ resource "azuread_app_role_assignment" "defender_machine_read" {
     for role in data.azuread_service_principal.defender.app_roles :
     role.id if role.value == "Machine.Read.All"
   ])
+}
+
+
+resource "azurerm_automation_runbook" "test_runbook" {
+
+  resource_group_name     = module.rg.rg_name
+  location                = module.rg.rg_location
+  tags                    = module.rg.rg_tags
+
+  automation_account_name = module.automation_account.aa_name
+  name                    = module.shared_vars.foundation_test_automation_runbook_name
+
+  runbook_type = "PowerShell"
+  log_verbose  = true
+  log_progress = true
+  description  = "Test stuff"
+
+  content = <<-EOF
+    $ErrorActionPreference = 'Stop'
+
+    function Get-ManagedIdentityToken {
+        param ([string] $Resource)
+
+        $uri = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2025-04-07&resource=$Resource"
+
+        $res = Invoke-RestMethod -Uri $uri -Headers @{ Metadata = 'true' }
+        return $res.access_token
+    }
+
+    Write-Host "Getting tokens..."
+
+    $graphToken = Get-ManagedIdentityToken "https://graph.microsoft.com"
+    $mdeToken   = Get-ManagedIdentityToken "https://api.securitycenter.microsoft.com"
+
+    Write-Host "Testing Graph access..."
+    $graph = Invoke-RestMethod `
+        -Uri "https://graph.microsoft.com/v1.0/devices" `
+        -Headers @{ Authorization = "Bearer $graphToken" }
+
+    Write-Host "Graph devices count: $($graph.value.Count)"
+
+    Write-Host "Testing MDE access..."
+    $mde = Invoke-RestMethod `
+        -Uri "https://api.securitycenter.microsoft.com/api/machines" `
+        -Headers @{ Authorization = "Bearer $mdeToken" }
+
+    Write-Host "MDE machines count: $($mde.value.Count)"
+  EOF
+}
+
+resource "azurerm_automation_schedule" "every_30_min" {
+  resource_group_name     = module.shared_vars.foundation_rg_name
+
+  automation_account_name = module.automation_account.aa_name
+  name                    = module.shared_vars.foundation_aa_every_30_min_schedule_name
+
+  frequency = "Minute"
+  interval  = 30
+  timezone  = "UTC"
+}
+
+resource "azurerm_automation_job_schedule" "runbook_schedule" {
+  resource_group_name     = module.shared_vars.foundation_rg_name
+
+  automation_account_name = module.automation_account.aa_name
+
+  runbook_name = azurerm_automation_runbook.test_runbook.name
+  schedule_name = azurerm_automation_schedule.every_30_min.name
 }
--- modules/shared_vars/naming.tf
+++ modules/shared_vars/naming.tf
@@ -2,10 +2,13 @@ locals {
   rg_prefix                             = "rg"
   user_assigned_managed_identity_prefix = "uid"
   automation_account_prefix             = "aa"
+  automation_account_runbook_prefix     = "aarb"
   base_name                             = "${local.normalised_short}-${local.validated_env}"
   foundation_rg_name                    = "${local.rg_prefix}-${local.base_name}-${local.normalised_layer}"
   foundation_uid_name                   = "${local.user_assigned_managed_identity_prefix}-${local.base_name}-${local.normalised_layer}"
   foundation_aa_name                    = "${local.automation_account_prefix}-${local.base_name}-${local.normalised_layer}"
+  foundation_test_automation_runbook_name = "${local.automation_account_runbook_prefix}-${local.base_name}-${local.normalised_layer}"
+  foundation_aa_every_30_min_schedule_name  = "Every-30-Minutes"
 }
 
 output "base_name" {
@@ -26,4 +29,14 @@ output "foundation_uid_name" {
 output "foundation_aa_name" {
   description = "Deterministic name for foundation aa"
   value       = local.foundation_aa_name
+}
+
+output "foundation_test_automation_runbook_name" {
+  description = "Deterministic name for foundation runbook name"
+  value       = local.foundation_test_automation_runbook_name
+}
+
+output "foundation_aa_every_30_min_schedule_name" {
+  description = "Deterministic name for foundation schedule name"
+  value       = local.foundation_aa_every_30_min_schedule_name
 }
